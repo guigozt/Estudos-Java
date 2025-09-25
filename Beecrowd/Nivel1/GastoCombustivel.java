9b9a18543baeac5baa241d2cc32aea5c2084e961
GUilherme <guilhermegomeszt@gmail.com>
2025-09-24 21:08:23 -0300

    add

---
+++ Beecrowd/Nivel1/GastoCombustivel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GastoCombustivel
+{
+    public static double quantLitros(int t, int v){
+        return (t * v) / 12.0;  
+    }
+    
+	public static void main(String[] args) {
+		try(Scanner entrada = new Scanner(System.in)){
+		    int tempo = entrada.nextInt();
+		    int velocidade = entrada.nextInt();
+		    
+		    System.out.printf("%.3f%n", quantLitros(tempo, velocidade));
+		}
+	}
+}
